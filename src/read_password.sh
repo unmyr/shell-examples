14f92b27c0f6de9208f916206fe8ffa97da98dcb
unmyr <56982541+unmyr@users.noreply.github.com>
2022-11-21 02:36:37 +0900

    feat: Add an example to read the password in terminal.

---
+++ src/read_password.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -s -p "Password: " MY_PASSWORD
+echo
+
+if [ -z "$MY_PASSWORD" ]; then
+    echo "ERROR: You have not entered your password. Stop processing." 1>&2
+    exit 1
+fi
+echo "PASS: $MY_PASSWORD"
