036cf011f8518dcd3279aa74aba7785793c3c2c9
unmyr <56982541+unmyr@users.noreply.github.com>
2024-12-27 21:37:50 +0900

    fix: Fix `read -r` example.

--- src/stdin/read_example.sh
+++ src/stdin/read_example.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
-INPUT_STR='Hello\nworld'
+INPUT_STR='Hello\world'
 read LINE < <(echo $INPUT_STR)
 echo $LINE  # output: Helloworld
 
 read -r LINE < <(echo $INPUT_STR)
-echo $LINE  # output: Hello\nworld
+echo $LINE  # output: Hello\world
