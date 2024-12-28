aece15e03c7b70cb5bbc01fc6879e13b269487ec
unmyr <56982541+unmyr@users.noreply.github.com>
2024-12-29 08:01:13 +0900

    refactor(src/array): Improve input data.

--- src/array/stdin_to_array_using_heredoc.sh
+++ src/array/stdin_to_array_using_heredoc.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 mapfile -t a <<'EOF'
-1
-2
-3
+foo
+bar
+Hello world
 EOF
 
 echo "size: ${#a[@]}"
--- src/array/stdin_to_array_using_tmpfile.sh
+++ src/array/stdin_to_array_using_tmpfile.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-TMP_FILE=$(mktemp -t $(basename "$0" .sh)XXX)
-trap "rm -f ${TMP_FILE}" 0
+TMP_FILE=$(mktemp -t $(basename "$0" .sh)XXX.tmp)
+trap "set -x; rm -f ${TMP_FILE}" 0
 
 cat > $TMP_FILE <<EOF
-1
-2
-3
+foo
+bar
+Hello world
 EOF
 
 mapfile -t a < $TMP_FILE
